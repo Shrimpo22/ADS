1d11948a0be66b8b160d5bd0ccab12fb75a9179a
Vasco Araújo <vasconparaujo@gmail.com>
2023-12-28 23:48:09 +0000

    Formulated Calendar Struct
    Calendar is a nested array structure, still requiring some tests but the calendar is calendar[year][month][day][hour] so if a class 'A' is on the 12/02/2022 from 14pm to 15pm then the calendar would have calendar['2022']['02']['12']['14:00'] = class A; calendar['2022']['02']['12']['14:30'] = class A;

--- index.js
+++ index.js
@@ -15,33 +15,138 @@ csvForm.addEventListener("submit", function (e) {
     reader.readAsText(rooms_input);
     reader.readAsText(schedule_input);
     */
+    let roomsPromiseResolved = false
+    let schedulePromiseResolved = false
 
-    create_objects(rooms_input, false)
+    const roomsPromise = create_objects(rooms_input, false)
         .then(roomsObjects => {
-            create_objects(schedule_input, false)
-                .then(scheduleObjects => {
-                    // Assign rooms to classes based on criteria
-                    //const assignedClasses = assignRoomsToClasses(scheduleObjects, roomsObjects);
-                    if(1 === 0) {
-                        for (const subjectName in roomsObjects) {
-                            if (roomsObjects.hasOwnProperty(subjectName)) {
-                                const subjectInfo = roomsObjects[subjectName];
-                                console.log(subjectInfo['Capacidade Normal'])
-                            }
-                        }
-                    }
-                    //console.log(roomsObjects)
-                    //console.log(scheduleObjects)
-                    assignRoomsToClasses(roomsObjects,scheduleObjects)
-                    // Print or use the assigned classes
-                    //console.log("Assigned Classes:", assignedClasses);
-                })
-                .catch(error => {
-                    console.error("Error creating schedule objects:", error);
-                });
+            roomsPromiseResolved = true;
+            return roomsObjects;
         })
         .catch(error => {
             console.error("Error creating rooms objects:", error);
+        });
+
+    const schedulePromise = create_objects(schedule_input, false)
+        .then(scheduleObjects => {
+            schedulePromiseResolved = true;
+            return scheduleObjects;
+        })
+        .catch(error => {
+            console.error("Error creating schedule objects:", error);
+        });
+
+    Promise.all([roomsPromise, schedulePromise])
+        .then(([roomsObjects, scheduleObjects]) => {
+            // Both create_objects calls completed
+            if (1 === 0) {
+                for (const subjectName in roomsObjects) {
+                    if (roomsObjects.hasOwnProperty(subjectName)) {
+                        const subjectInfo = roomsObjects[subjectName];
+                        console.log(subjectInfo['Capacidade Normal']);
+                    }
+                }
+            }
+            const resultRoomsContainer = document.getElementById('result-rooms-container');
+            const resultScheduleContainer = document.getElementById('result-schedule-container');
+
+            printObjectsTable(roomsObjects, resultRoomsContainer)
+            printObjectsTable(scheduleObjects, resultScheduleContainer)
+
+            assignRoomsToClasses(roomsObjects, scheduleObjects);
+        })
+        .catch(error => {
+            console.error("Error creating objects:", error);
+        });
+
+    //while (!(roomsPromiseResolved && schedulePromiseResolved)) {
+    //Se eventualmente quisermos fazer coisas enquanto as promisses não estiverem resolved
+    //}
+
+    Promise.all([roomsPromise, schedulePromise])
+        .then(([roomsObjects, scheduleObjects]) => {
+           const calendar = {}
+            for (const ro of scheduleObjects) {
+                console.log(ro)
+                const date = ro['Dia'];
+                const startTime = ro['Início']
+                const endTime = ro['Fim']
+
+                console.log(date)
+                console.log(startTime)
+                console.log(endTime)
+
+                const [day, month, year] = date.split('/');
+                let hourStart = startTime.split(':').slice(0, 2).join(':');
+                console.log("HourStart: " + hourStart)
+
+                // Check if the year exists in the calendar, if not, add it
+                if (!calendar[year]) {
+                    console.log(year)
+                    calendar[year] = {};
+                }
+
+                // Check if the month exists in the year, if not, add it
+                if (!calendar[year][month]) {
+                    console.log(month)
+                    calendar[year][month] = {};
+                }
+
+                // Check if the day exists in the month, if not, add it
+                if (!calendar[year][month][day]) {
+                    console.log(day)
+                    calendar[year][month][day] = {};
+                }
+
+                if (!calendar[year][month][day][hourStart]) {
+                    console.log(hourStart)
+                    calendar[year][month][day][hourStart] = {};
+                }
+
+                let hour = hourStart
+                console.log(endTime)
+                while(compareTimes(hour, endTime)){
+                    const [hours, minutes] = hour.split(':').map(Number);
+                    const newMinutes = (minutes + 30) % 60;
+                    const newHours = (hours + Math.floor((minutes + 30) / 60)) % 24;
+
+                    console.log("Minutes: "+minutes+" => " +newMinutes+ " Hours: "+hours+" => "+newHours)
+                    hour = `${String(newHours).padStart(2, '0')}:${String(newMinutes).padStart(2, '0')}`;
+
+                    console.log("New Hour: "+hour);
+
+
+                    let num = 0;
+                    if (!calendar[year][month][day][hour]) {
+                        console.log(hour)
+                        calendar[year][month][day][hour] = num + "a";
+                        calendar[year][month][day][hour] += num + "b";
+                        num++
+                    }
+                }
+                console.log(calendar['2022']['12']['02'])
+                function compareTimes(time1, time2) {
+                    console.log("Compare "+time1+" "+ time2)
+                    // Convert times to minutes since midnight
+                    const [hours1, minutes1] = time1.split(':').map(Number);
+                    const [hours2, minutes2] = time2.split(':').map(Number);
+
+                    const totalMinutes1 = hours1 * 60 + minutes1;
+                    const totalMinutes2 = hours2 * 60 + minutes2;
+
+                    // Compare the numeric values
+                    if (totalMinutes1 < totalMinutes2) {
+                        return -1;
+                    } else if (totalMinutes1 > totalMinutes2) {
+                        return 1;
+                    } else {
+                        return 0; // Times are equal
+                    }
+                }
+            }
+        })
+        .catch(error => {
+            console.error("Error creating objects:", error);
         });
 });
 
@@ -56,22 +161,23 @@ function create_objects(csvFile, debug) {
             // For simplicity, let's assume each line is an object
             const lines = text.split('\n');
             const headers = lines[0].replace(/\r$/, '').split(';');
-            if(debug === true)
+            if (debug === true)
                 console.log(headers)
             lines.slice(1).forEach((line, index) => {
                 const values = line.replace(/\r$/, '').split(';');
                 const debug_value = `${values.join(' | ')} (Line ${index + 2})`;
-                if(debug === true)
-                  console.log(debug_value)
+                if (debug === true)
+                    console.log(debug_value)
                 const obj = {};
                 let p = 0;
-                for ( const header of headers)
-                {
+                for (const header of headers) {
                     if (values[p] !== '')
                         obj[header] = values[p]
                     p++;
                 }
 
+                if (debug === true)
+                    console.log(obj)
                 objects.push(obj);
             });
 
@@ -83,11 +189,12 @@ function create_objects(csvFile, debug) {
     });
 }
 
-function assignRoomsToClasses(roomsObjects, scheduleObjects){
+function assignRoomsToClasses(roomsObjects, scheduleObjects, debug) {
+    const resultContainer = document.getElementById('result-match-container');
 
     //criar um hashmap (com sala, hora alocada)  aqui no início para adicionar as salas que nao estao disponivies. Após alocarmos uma sala, adicionamos a este map, e sempre que quisermos alocar uma sala vamos verificar neste map se a sala àquela hora nao se encontra no map
     // ter uma var tolerancia
-    for(const so of scheduleObjects){
+    for (const so of scheduleObjects) {
         const requiredCapacity = so['Inscritos no turno'];
         const requestedClass = so['Características da sala pedida para a aula']
         //console.log(requestedClass)
@@ -96,11 +203,82 @@ function assignRoomsToClasses(roomsObjects, scheduleObjects){
             .filter(ro => ro['Capacidade Normal'] >= requiredCapacity && ro[requestedClass] === 'X')
             .map(ro => ro['Nome sala']);
 
+        const requirementsList = so['Características da sala pedida para a aula']
+        const listItems = matchingRooms.map(room => `<li>${room}</li>`).join('');
 
-        console.log(`Para a Unidade de execução ${so['Unidade de execução']} no dia ${so['Dia']} que precisa de ${so['Inscritos no turno']} lugares temos as seguinte salas disponíveis`);
-        console.log('Salas com os requerimentos solicitados:', matchingRooms);
+        const htmlContent = `
+            <h2>${so['Unidade de execução']} no dia ${so['Dia']} que precisa de ${so['Inscritos no turno']} lugares:</h2>
+            <ul style="color: cornflowerblue;">${requirementsList}</ul>
+            <ul style="color: indianred;">${listItems}</ul>
+        `;
+
+        // Create a new div element to hold the HTML content
+        const divElement = document.createElement('div');
+        divElement.classList.add('box'); // add the 'box' class to the created div
+        divElement.innerHTML = htmlContent;
+
+        // Append the div to the result container
+        resultContainer.appendChild(divElement);
+
+        if (debug === true) {
+            console.log(`Para a Unidade de execução ${so['Unidade de execução']} no dia ${so['Dia']} que precisa de ${so['Inscritos no turno']} lugares temos as seguinte salas disponíveis`);
+            console.log('Salas com os requerimentos solicitados:', matchingRooms);
+        }
+    }
+}
+
+function printObjectsTable(objObjects, resultContainer) {
+    // Check if there are rooms to display
+    if (objObjects.length === 0) {
+        resultContainer.innerHTML = '<p>No rooms available</p>';
+        return;
     }
 
+    // Collect all unique keys (columns) from all objects
+    const allKeys = objObjects.reduce((keys, obj) => {
+        Object.keys(obj).forEach(key => {
+            if (!keys.includes(key)) {
+                keys.push(key);
+            }
+        });
+        return keys;
+    }, []);
 
+    // Create the table element
+    const table = document.createElement('table');
+    table.style.borderCollapse = 'collapse'; // Add border-collapse style
+
+    // Create the table header
+    const thead = document.createElement('thead');
+    const headerRow = document.createElement('tr');
+    allKeys.forEach(key => {
+        const th = document.createElement('th');
+        th.textContent = key;
+        th.style.border = '1px solid #dddddd'; // Add border style
+        th.style.padding = '8px'; // Add padding for better visibility
+        headerRow.appendChild(th);
+    });
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
+
+    // Create the table body
+    const tbody = document.createElement('tbody');
+    objObjects.forEach(obj => {
+        const row = document.createElement('tr');
+        allKeys.forEach(key => {
+            const cell = document.createElement('td');
+            cell.textContent = obj[key] || ''; // Handle cases where a key is not present in an object
+            cell.style.border = '1px solid #dddddd'; // Add border style
+            cell.style.padding = '8px'; // Add padding for better visibility
+            row.appendChild(cell);
+        });
+        tbody.appendChild(row);
+    });
+    table.appendChild(tbody);
+
+    // Append the table to the result container
+    resultContainer.innerHTML = ''; // Clear existing content
+    resultContainer.appendChild(table);
 }
+
 
