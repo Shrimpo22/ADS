1293ab24fe75ea7a83f9f36177f01e7a74ddfafc
Vasco Araújo <vasconparaujo@gmail.com>
2023-12-29 16:02:43 +0000

    Calendar Struct Done
    Added javadoc style comments and created markAsUnavailable

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
+/**
+ * Represents the form and input elements for CSV files.
+ * @type {HTMLFormElement}
+ */
 const csvForm = document.getElementById("csvForm");
+
+/**
+ * Represents the input element for rooms CSV file.
+ * @type {HTMLInputElement}
+ */
 const roomsCSV = document.getElementById("roomsCSV");
+
+/**
+ * Represents the input element for schedule CSV file.
+ * @type {HTMLInputElement}
+ */
 const scheduleCSV = document.getElementById("scheduleCSV");
 
+/**
+ * Object to store calendar data.
+ * @type {Object}
+ */
+const calendar = {}
+
+/**
+ * Event listener for form submission to handle CSV file processing.
+ */
 csvForm.addEventListener("submit", function (e) {
     e.preventDefault();
     const rooms_input = roomsCSV.files[0];
@@ -54,6 +77,7 @@ csvForm.addEventListener("submit", function (e) {
             printObjectsTable(scheduleObjects, resultScheduleContainer)
 
             assignRoomsToClasses(roomsObjects, scheduleObjects);
+            markAsUnavailable("Catacumbas", '02/12/2022', '12:00:00', '14:30:00', true)
         })
         .catch(error => {
             console.error("Error creating objects:", error);
@@ -62,94 +86,15 @@ csvForm.addEventListener("submit", function (e) {
     //while (!(roomsPromiseResolved && schedulePromiseResolved)) {
     //Se eventualmente quisermos fazer coisas enquanto as promisses não estiverem resolved
     //}
-
-    Promise.all([roomsPromise, schedulePromise])
-        .then(([roomsObjects, scheduleObjects]) => {
-           const calendar = {}
-            for (const ro of scheduleObjects) {
-                console.log(ro)
-                const date = ro['Dia'];
-                const startTime = ro['Início']
-                const endTime = ro['Fim']
-
-                console.log(date)
-                console.log(startTime)
-                console.log(endTime)
-
-                const [day, month, year] = date.split('/');
-                let hourStart = startTime.split(':').slice(0, 2).join(':');
-                console.log("HourStart: " + hourStart)
-
-                // Check if the year exists in the calendar, if not, add it
-                if (!calendar[year]) {
-                    console.log(year)
-                    calendar[year] = {};
-                }
-
-                // Check if the month exists in the year, if not, add it
-                if (!calendar[year][month]) {
-                    console.log(month)
-                    calendar[year][month] = {};
-                }
-
-                // Check if the day exists in the month, if not, add it
-                if (!calendar[year][month][day]) {
-                    console.log(day)
-                    calendar[year][month][day] = {};
-                }
-
-                if (!calendar[year][month][day][hourStart]) {
-                    console.log(hourStart)
-                    calendar[year][month][day][hourStart] = {};
-                }
-
-                let hour = hourStart
-                console.log(endTime)
-                while(compareTimes(hour, endTime)){
-                    const [hours, minutes] = hour.split(':').map(Number);
-                    const newMinutes = (minutes + 30) % 60;
-                    const newHours = (hours + Math.floor((minutes + 30) / 60)) % 24;
-
-                    console.log("Minutes: "+minutes+" => " +newMinutes+ " Hours: "+hours+" => "+newHours)
-                    hour = `${String(newHours).padStart(2, '0')}:${String(newMinutes).padStart(2, '0')}`;
-
-                    console.log("New Hour: "+hour);
-
-
-                    let num = 0;
-                    if (!calendar[year][month][day][hour]) {
-                        console.log(hour)
-                        calendar[year][month][day][hour] = num + "a";
-                        calendar[year][month][day][hour] += num + "b";
-                        num++
-                    }
-                }
-                console.log(calendar['2022']['12']['02'])
-                function compareTimes(time1, time2) {
-                    console.log("Compare "+time1+" "+ time2)
-                    // Convert times to minutes since midnight
-                    const [hours1, minutes1] = time1.split(':').map(Number);
-                    const [hours2, minutes2] = time2.split(':').map(Number);
-
-                    const totalMinutes1 = hours1 * 60 + minutes1;
-                    const totalMinutes2 = hours2 * 60 + minutes2;
-
-                    // Compare the numeric values
-                    if (totalMinutes1 < totalMinutes2) {
-                        return -1;
-                    } else if (totalMinutes1 > totalMinutes2) {
-                        return 1;
-                    } else {
-                        return 0; // Times are equal
-                    }
-                }
-            }
-        })
-        .catch(error => {
-            console.error("Error creating objects:", error);
-        });
 });
 
+/**
+ * Reads a CSV file and creates objects based on its content.
+ *
+ * @param {File} csvFile - The CSV file to be processed.
+ * @param {boolean} debug - Flag to enable debugging output.
+ * @returns {Promise<Array<Object>>} A promise that resolves with an array of objects.
+ */
 function create_objects(csvFile, debug) {
     const reader = new FileReader();
     const objects = [];
@@ -189,6 +134,14 @@ function create_objects(csvFile, debug) {
     });
 }
 
+
+/**
+ * Assigns rooms to classes based on specified criteria.
+ *
+ * @param {Array<Object>} roomsObjects - Array of room objects.
+ * @param {Array<Object>} scheduleObjects - Array of schedule objects.
+ * @param {boolean} debug - Flag to enable debugging output.
+ */
 function assignRoomsToClasses(roomsObjects, scheduleObjects, debug) {
     const resultContainer = document.getElementById('result-match-container');
 
@@ -197,7 +150,6 @@ function assignRoomsToClasses(roomsObjects, scheduleObjects, debug) {
     for (const so of scheduleObjects) {
         const requiredCapacity = so['Inscritos no turno'];
         const requestedClass = so['Características da sala pedida para a aula']
-        //console.log(requestedClass)
 
         const matchingRooms = roomsObjects
             .filter(ro => ro['Capacidade Normal'] >= requiredCapacity && ro[requestedClass] === 'X')
@@ -227,6 +179,12 @@ function assignRoomsToClasses(roomsObjects, scheduleObjects, debug) {
     }
 }
 
+/**
+ * Prints a table of objects to a specified result container.
+ *
+ * @param {Array<Object>} objObjects - Array of objects to be displayed.
+ * @param {HTMLElement} resultContainer - HTML element to display the table.
+ */
 function printObjectsTable(objObjects, resultContainer) {
     // Check if there are rooms to display
     if (objObjects.length === 0) {
@@ -281,4 +239,65 @@ function printObjectsTable(objObjects, resultContainer) {
     resultContainer.appendChild(table);
 }
 
+/**
+ * Marks a room as unavailable during a specified time range.
+ *
+ * @param {string} roomName - The name of the room to be marked as unavailable.
+ * @param {string} date - The date for which the room is marked as unavailable.
+ * @param {string} startTime - The start time of the unavailability period.
+ * @param {string} endTime - The end time of the unavailability period.
+ * @param {boolean} debug - Flag to enable debugging output.
+ */
+function markAsUnavailable(roomName, date, startTime, endTime, debug){
+
+        const [day, month, year] = date.split('/');
+        let hourStart = startTime.split(':').slice(0, 2).join(':');
+
+        debug && console.log("HourStart: " + hourStart);
+
+        // Check if the year exists in the calendar, if not, add it
+        calendar[year] = calendar[year] || {};
+
+        // Check if the month exists in the year, if not, add it
+        calendar[year][month] = calendar[year][month] || {};
+
+        // Check if the day exists in the month, if not, add it
+        calendar[year][month][day] = calendar[year][month][day] || {};
+
+        let currHour = hourStart
+        while(compareTimes(currHour, endTime)){
+            debug && console.log("New Hour: "+currHour);
+
+            calendar[year][month][day][currHour] = calendar[year][month][day][currHour] || [];
+
+            calendar[year][month][day][currHour].push(roomName);
+
+            const [hours, minutes] = currHour.split(':').map(Number);
+            const newMinutes = (minutes + 30) % 60;
+            const newHours = (hours + Math.floor((minutes + 30) / 60)) % 24;
+
+            debug && console.log("Minutes: "+minutes+" => " +newMinutes+ " Hours: "+hours+" => "+newHours)
+            currHour = `${String(newHours).padStart(2, '0')}:${String(newMinutes).padStart(2, '0')}`;
+
+        }
+        debug && console.log(calendar['2022']['12']['02'])
+        function compareTimes(time1, time2) {
+            debug && console.log("Compare "+time1+" "+ time2)
+            // Convert times to minutes since midnight
+            const [hours1, minutes1] = time1.split(':').map(Number);
+            const [hours2, minutes2] = time2.split(':').map(Number);
+
+            const totalMinutes1 = hours1 * 60 + minutes1;
+            const totalMinutes2 = hours2 * 60 + minutes2;
+
+            // Compare the numeric values
+            if (totalMinutes1 < totalMinutes2) {
+                return -1;
+            } else if (totalMinutes1 > totalMinutes2) {
+                return 1;
+            } else {
+                return 0; // Times are equal
+            }
+        }
+}
 
